35797149e7996d7a8bd612349b71acf26caa24bf
Ettore Di Giacinto <edigiacinto@suse.com>
2019-09-01 16:27:04 +0200

    Consume URL from current location

--- ui/src/ekcpdataProvider.js
+++ ui/src/ekcpdataProvider.js
@@ -5,7 +5,7 @@ import {
     CREATE,
     DELETE,
 } from 'react-admin';
-const apiUrl = process.env.KUBEHOST || 'http://localhost:9090';
+const apiUrl = window.location.protocol+'//'+window.location.hostname+(window.location.port ? ':'+window.location.port: '');
 
 /**
  * Maps react-admin queries to my REST API
